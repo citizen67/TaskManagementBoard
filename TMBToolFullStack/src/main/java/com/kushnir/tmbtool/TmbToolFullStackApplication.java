047b5f149584211a9b61edba92b4d3d817305e08
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2021-12-20 02:01:27 +0200

    first commit

---
+++ TMBToolFullStack/src/main/java/com/kushnir/tmbtool/TmbToolFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.kushnir.tmbtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmbToolFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmbToolFullStackApplication.class, args);
+    }
+
+}
---
+++ TMBToolFullStack/src/test/java/com/kushnir/tmbtool/TmbToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushnir.tmbtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmbToolFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
