a99f6e148957ca88ece876b9e4bd8c7bd5cac1e4
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2021-12-27 14:51:22 +0200

    Delete existing project by Id

--- TMBToolFullStack/src/main/java/com/kushnir/tmbtool/services/ProjectService.java
+++ TMBToolFullStack/src/main/java/com/kushnir/tmbtool/services/ProjectService.java
@@ -36,4 +36,14 @@ public class ProjectService {
     public Iterable<Project> findAllProjects() {
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if (project == null) {
+            throw new ProjectIdException("Cannot delete Project with ID '" + projectId + "'. This project does not exists.");
+        }
+
+        projectRepository.delete(project);
+    }
 }
