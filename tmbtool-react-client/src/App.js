50a567fd79d28d8924cdf581b5e7c8b6169cbbc4
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2022-01-02 16:49:30 +0200

    Add React Router V6

--- tmbtool-react-client/src/App.js
+++ tmbtool-react-client/src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
-    <div className="App">
+    // Upgrading to React Router V6
+    <BrowserRouter>
       <Header />
-      <Dashboard />
-    </div>
+      <Routes>
+        {/* This part need for pre-load main App page */}
+        <Route path="/" element={<Dashboard />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addProject" element={<AddProject />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
