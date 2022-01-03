4df7197373a40e7ee7c284eed324f3b8fc36ee6a
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2022-01-03 02:08:27 +0200

    Create Redux store

--- tmbtool-react-client/src/App.js
+++ tmbtool-react-client/src/App.js
@@ -4,20 +4,24 @@ import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddProject from './components/Project/AddProject';
+import { Provider } from "react-redux";
+import store from './store';
 
 function App() {
   return (
-    // Upgrading to React Router V6
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        {/* This part need for pre-load main App page */}
-        <Route path="/" element={<Dashboard />} />
+    <Provider store={store}>
+      {/* Upgrading to React Router V6 */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/* This route included for pre-load main App page */}
+          <Route path="/" element={<Dashboard />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/addProject" element={<AddProject />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/addProject" element={<AddProject />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
