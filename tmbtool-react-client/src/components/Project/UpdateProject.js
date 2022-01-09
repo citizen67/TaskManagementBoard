955c4cdbd36e4cb79b970175a0b83e74ff3643b7
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2022-01-09 03:16:28 +0200

    Get project bt id, update case

--- tmbtool-react-client/src/components/Project/UpdateProject.js
+++ tmbtool-react-client/src/components/Project/UpdateProject.js
@@ -1,6 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { getProject } from '../../actions/projectActions';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classNames from "classnames";
 
-export default class UpdateProject extends Component {
+class UpdateProject extends Component {
+
+    //set state
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        this.props.getProject(id, this.props.history);
+    }
+
     render() {
         return (
             <div className="project">
@@ -60,3 +71,14 @@ export default class UpdateProject extends Component {
         )
     }
 }
+
+UpdateProject.PropTypes = {
+    getProject: PropTypes.func.isRequired,
+    project: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    project: state.project.project
+});
+
+export default connect(mapStateToProps, { getProject })(UpdateProject);
