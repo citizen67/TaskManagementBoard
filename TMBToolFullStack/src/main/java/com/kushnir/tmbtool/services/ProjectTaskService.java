b979644d7e26b34b335303fef44349ba68e7d4aa
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2022-01-13 00:19:34 +0200

    Get project backlog

--- TMBToolFullStack/src/main/java/com/kushnir/tmbtool/services/ProjectTaskService.java
+++ TMBToolFullStack/src/main/java/com/kushnir/tmbtool/services/ProjectTaskService.java
@@ -48,4 +48,7 @@ public class ProjectTaskService {
         return projectTaskRepository.save(projectTask);
     }
 
+    public Iterable<ProjectTask> findBacklogById(String id) {
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
+    }
 }
