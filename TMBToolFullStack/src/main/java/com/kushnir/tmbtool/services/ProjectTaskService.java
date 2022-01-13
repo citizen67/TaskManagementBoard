eb05ac907c275dfaffc80689dbfee9b2eef68a37
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2022-01-13 16:38:17 +0200

    Enforce find by ProjectTask sequence

--- TMBToolFullStack/src/main/java/com/kushnir/tmbtool/services/ProjectTaskService.java
+++ TMBToolFullStack/src/main/java/com/kushnir/tmbtool/services/ProjectTaskService.java
@@ -61,7 +61,7 @@ public class ProjectTaskService {
         Project project = projectRepository.findByProjectIdentifier(id);
 
         if (project == null) {
-            throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist");
+            throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist.");
         }
 
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
@@ -69,6 +69,23 @@ public class ProjectTaskService {
 
     public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
 
-        return projectTaskRepository.findByProjectSequence(pt_id);
+        //make sure we are searching for an existing project
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+        if (backlog == null) {
+            throw new ProjectNotFoundException("Project with ID: '" + backlog_id + "' does not exist.");
+        }
+
+        //make sure that task exists
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        if (projectTask == null) {
+            throw new ProjectNotFoundException("Project Task '" + pt_id + "' not found.");
+        }
+
+        //make sure that the backlog/project id in the path corresponds to the right project
+        if (!projectTask.getProjectIdentifier().equals(backlog_id)) {
+            throw new ProjectNotFoundException("Project Task '" + pt_id + "' does not exist in project: " + backlog_id);
+        }
+
+        return projectTask;
     }
 }
