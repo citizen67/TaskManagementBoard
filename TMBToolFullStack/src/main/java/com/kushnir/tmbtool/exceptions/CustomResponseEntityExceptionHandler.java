cb43491756aba4065428b345e55cc13e268b5529
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2022-01-13 02:34:02 +0200

    Handle project not found exception, switch to use MySql

--- TMBToolFullStack/src/main/java/com/kushnir/tmbtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ TMBToolFullStack/src/main/java/com/kushnir/tmbtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -17,4 +17,10 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request) {
+        ProjectNotFoundExceptionResponce projectNotFoundExceptionResponce = new ProjectNotFoundExceptionResponce(ex.getMessage());
+        return new ResponseEntity(projectNotFoundExceptionResponce, HttpStatus.BAD_REQUEST);
+    }
 }
--- TMBToolFullStack/src/main/java/com/kushnir/tmbtool/services/ProjectTaskService.java
+++ TMBToolFullStack/src/main/java/com/kushnir/tmbtool/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package com.kushnir.tmbtool.services;
 
 import com.kushnir.tmbtool.domain.Backlog;
+import com.kushnir.tmbtool.domain.Project;
 import com.kushnir.tmbtool.domain.ProjectTask;
+import com.kushnir.tmbtool.exceptions.ProjectNotFoundException;
 import com.kushnir.tmbtool.repositories.BacklogRepository;
+import com.kushnir.tmbtool.repositories.ProjectRepository;
 import com.kushnir.tmbtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,39 +19,51 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
 
-        //Exceptions: Project not found
+        try {
+            //PTs to be added to a specific project, project != null, BL exists
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            //Set the Backlog to the PT
+            projectTask.setBacklog(backlog);
 
-        //PTs to be added to a specific project, project != null, BL exists
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        //Set the Backlog to the PT
-        projectTask.setBacklog(backlog);
+            Integer backlogSequence = backlog.getPTSequence();
+            //Update the Backlog SEQUENCE
+            backlogSequence++;
 
-        Integer backlogSequence = backlog.getPTSequence();
-        //Update the Backlog SEQUENCE
-        backlogSequence++;
+            backlog.setPTSequence(backlogSequence);
 
-        backlog.setPTSequence(backlogSequence);
+            //Add Sequence to the Project Task
+            projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        //Add Sequence to the Project Task
-        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            //INITIAL priority when priority null
+            if (projectTask.getPriority() == null) {
+                projectTask.setPriority(3);
+            }
 
-        //INITIAL priority when priority null
-        if (projectTask.getPriority() == null) {
-            projectTask.setPriority(3);
+            //INITIAL status when status in null
+            if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+                projectTask.setStatus("TO_DO");
+            }
+
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project not found.");
         }
-
-        //INITIAL status when status in null
-        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
-            projectTask.setStatus("TO_DO");
-        }
-
-        return projectTaskRepository.save(projectTask);
     }
 
     public Iterable<ProjectTask> findBacklogById(String id) {
+
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if (project == null) {
+            throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist");
+        }
+
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
