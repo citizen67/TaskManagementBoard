f749f407e8efddf2511e823a7d6d8370c21141f9
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2022-01-17 23:40:00 +0200

    Update project task ot be in the state

--- tmbtool-react-client/src/actions/backlogActions.js
+++ tmbtool-react-client/src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_BACKLOG, GET_ERRORS } from "./types";
+import { GET_BACKLOG, GET_ERRORS, GET_PROJECT_TASK } from "./types";
 
 export const addProjectTask = (backlog_id, project_task, history) => async dispatch => {
 
@@ -34,4 +34,18 @@ export const getBacklog = backlog_id => async dispatch => {
         });
     }
 
-}
+};
+
+export const getProjectTask = (backlog_id, pt_id, history) => async dispatch => {
+
+    try {
+        const res = await axios.get(`/api/backlog/${backlog_id}/${pt_id}`);
+        dispatch({
+            type: GET_PROJECT_TASK,
+            payload: res.date
+        })
+    } catch (err) {
+        history.push("/dashboard");
+    }
+
+};
