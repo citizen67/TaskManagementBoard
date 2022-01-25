422caf8a4a9de51b5b72a13232b8a79b2ef28df5
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2022-01-25 23:34:31 +0200

    Add exception handling for user isn't unique case

--- backend/src/main/java/com/kushnir/tmbtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ backend/src/main/java/com/kushnir/tmbtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -23,4 +23,10 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ProjectNotFoundExceptionResponce projectNotFoundExceptionResponce = new ProjectNotFoundExceptionResponce(ex.getMessage());
         return new ResponseEntity(projectNotFoundExceptionResponce, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex, WebRequest request) {
+        UsernameAlreadyExistsResponse exceptionResponse = new UsernameAlreadyExistsResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 }
--- backend/src/main/java/com/kushnir/tmbtool/services/UserService.java
+++ backend/src/main/java/com/kushnir/tmbtool/services/UserService.java
@@ -1,6 +1,7 @@
 package com.kushnir.tmbtool.services;
 
 import com.kushnir.tmbtool.domain.User;
+import com.kushnir.tmbtool.exceptions.UsernameAlreadyExistsException;
 import com.kushnir.tmbtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,11 +17,18 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser(User newUser) {
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-        //Username need to be unique
+        try {
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-        return userRepository.save(newUser);
+            //Username need to be unique
+            newUser.setUsername(newUser.getUsername());
+
+            return userRepository.save(newUser);
+        } catch (Exception e) {
+            throw new UsernameAlreadyExistsException("Username '" + newUser.getUsername() + "' already exists.");
+        }
+
     }
 
 }
