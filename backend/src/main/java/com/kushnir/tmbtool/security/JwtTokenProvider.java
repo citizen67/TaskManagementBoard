d5eed41fce5e3e8c3f91efab0064ac784002b822
Volodymyr Kushnir <dev.kushnir@yahoo.com>
2022-01-27 18:40:39 +0200

    Add jwt login token generation

---
+++ backend/src/main/java/com/kushnir/tmbtool/security/JwtTokenProvider.java
@@ -0,0 +1,40 @@
+package com.kushnir.tmbtool.security;
+
+import com.kushnir.tmbtool.domain.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.kushnir.tmbtool.security.SecurityConstants.SECRET;
+import static com.kushnir.tmbtool.security.SecurityConstants.TOKEN_EXPIRATION_TIME;
+
+@Component
+public class JwtTokenProvider {
+
+    //Generate the token
+    public String generateToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date expiryDate = new Date(now.getTime() + TOKEN_EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", (Long.toString(user.getId())));
+        claims.put("username", user.getUsername());
+        claims.put("fullName", user.getFullName());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+}
